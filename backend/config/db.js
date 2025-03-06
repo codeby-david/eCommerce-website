e61388688bbd30300f1582dfcb529c0b80dba9cc
DavidM <169084424+codeby-david@users.noreply.github.com>
2025-03-06 15:06:44 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose"
 
 export const connectDB = async () =>{
+    try{
     await mongoose.connect('mongodb+srv://davidmacharia:08220201002@cluster0.seunwbt.mongodb.net/food-delivery').then(()=>{
-        console.log("connected to db")
+        console.log("connected to db");
+    }catch(err){
+        res.status(500).json({message.err.message});
     });
 }
